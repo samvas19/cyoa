bb5ec35fd0beb506ed13f8e598eeb7db90a1b4a9
Samuel Vasquez <samuelvasquez@Sams-Macbook-Pro.local>
2019-03-02 13:16:39 -0800

    more pages

---
+++ resources/js/main.js
@@ -0,0 +1,64 @@
+$(document).on('click', '#choice1', function(){
+
+	localStorage.setItem('direction', 'choice1');
+
+	window.location.href = "././choice1.html";
+
+});
+
+
+$(document).on('click', '#choice2', function(){
+
+	localStorage.setItem('direction', 'choice2');
+
+	window.location.href = "././choice2.html";
+
+});
+
+
+$(document).on('click', '#choice3', function(){
+
+	localStorage.setItem('direction', 'choice3');
+	
+	window.location.href = "././choice3.html";
+
+});
+
+$(document).on('click', '#hide', function(){
+
+	var currentDirection = localStorage.getItem('direction');
+
+	if ( currentDirection === 'choice1' ) {
+		window.location.href = "././bear-is-ok.html";
+		
+	} 
+
+	else if ( currentDirection === 'choice2' ) {
+		window.location.href = "././bear-bad.html";
+	}
+
+	else if ( currentDirection === 'choice3' ) {
+		window.location.href = "././bear-bad.html";
+	}
+
+});
+
+
+$(document).on('click', '#lake', function(){
+
+	var currentDirection = localStorage.getItem('direction');
+
+	if ( currentDirection === 'choice1' ) {
+		window.location.href = "././explorelake.html";
+		
+	} 
+
+	else if ( currentDirection === 'choice2' ) {
+		window.location.href = "././foodatlake.html";
+	}
+
+	else if ( currentDirection === 'choice3' ) {
+		window.location.href = "././foodatlake.html";
+	}
+
+});
